8d4b03476f52874156dc87f399709e9df3de73ba
Abhay <abhaypvt2903@gmail.com>
2026-03-03 00:38:07 +0530

    Add done working on profile and fetch postbyuser in profile

--- posts.js
+++ posts.js
@@ -17,3 +17,54 @@ if(authLinks && userLinks){
         userLinks.style.display= "none";
     }
 }
+
+document.addEventListener("DOMContentLoaded", fetchAllPosts);
+
+async function fetchAllPosts() {
+    try{
+        const response = await fetch(
+        "http://blog-app-env.eba-axajqaxr.eu-north-1.elasticbeanstalk.com/api/posts"
+        );
+        if(!response.ok){
+            throw new console.error("Failed to fetch Posts");
+        }
+        const data = await response.json();
+
+        renderPosts(data.content);
+    }catch (error){
+        console.error("Error", error);
+        alert("Error in loading Posts");
+    }
+}
+function renderPosts(posts){
+    const container = document.getElementById("postsContainer");
+    container.innerHTML= "";
+
+    if(!posts || posts.length ===0){
+    container.innerHTML = "<p>No posts available</p>"
+    return
+    }
+
+    posts.forEach(post => {
+        const div = document.createElement("div");
+        div.classList.add("post-card");
+
+        div.innerHTML= `
+        <div class="pot-body">
+        <h2> Title: ${post.title}</h2>
+        <br/>
+        <h4>Content:</h4><p>${post.content}</p>
+        <br/>
+        </div>
+        <div class="post-footer">
+        
+        Category:${post.category.categoryTitle}
+        <span class="author">👤 ${post.user.name}</span>
+        <span class="date">📅 ${new Date(post.addedDate).toDateString()}</span>
+        
+        <hr/>
+        </div>
+        `;
+        container.appendChild(div);
+    });
+}
--- profile.js
+++ profile.js
@@ -1,5 +1,101 @@
 const token = localStorage.getItem("token");
 
-if(!token){
-    window.location.href="login.html";
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+
+    if (!token || !userId) {
+    alert("Please login first");
+    window.location.replace("login.html");
+    return;
+}
+    fetchProfile(userId, token);
+    fetchUserPosts(userId, token);
+});
+
+async function fetchProfile(userId, token) {
+
+
+    try {
+    const response = await fetch(
+        `http://Blog-app-env.eba-axajqaxr.eu-north-1.elasticbeanstalk.com/api/users/${userId}`,
+        {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }
+    );
+
+    if (!response.ok) {
+        throw new Error("Failed to load profile");
+    }
+
+    const user = await response.json();
+
+    document.getElementById("name").innerText = user.name;
+    document.getElementById("email").innerText = user.email;
+    document.getElementById("about").innerText = user.about || "—";
+    document.getElementById("role").innerText = user.roles[0]?.name || "USER";
+
+    } catch (error) {
+    alert(error.message);
+    }
+}
+
+        // Fetch User Posts
+async function fetchUserPosts(userId,token) {
+
+    if(!token){
+    alert("Please Login first");
+    window.location.replace("login.html");
+    return;
+    }
+
+    
+    const response = await fetch(
+        `http://Blog-app-env.eba-axajqaxr.eu-north-1.elasticbeanstalk.com/api/user/${userId}/posts`,
+        {
+            headers:{
+                "Authorization": `Bearer ${token}`
+            }
+        }
+    );
+    const data = await response.json();
+    const posts = data.content;
+    renderPosts(posts);
+}
+
+
+function renderPosts(posts){
+    const container = document.getElementById("postsContainer");
+    container.innerHTML= "";
+
+    if(!posts || posts.length ===0){
+    container.innerHTML = "<p>No posts available</p>"
+    return
+    }
+
+    posts.forEach(post => {
+        const div = document.createElement("div");
+        div.classList.add("post-card");
+
+        div.innerHTML= `
+        <div class="post-body">
+        <h2> Title: ${post.title}</h2>
+        <br/>
+        <h4>Content:</h4><p>${post.content}</p>
+        <br/>
+        </div>
+        <div class="post-footer">
+        
+        Category:${post.category.categoryTitle}
+        <span class="author">👤 ${post.user.name}</span>
+        <span class="date">📅 ${new Date(post.addedDate).toDateString()}</span>
+        
+        <hr/>
+        </div>
+        `;
+        container.appendChild(div);
+    });
 }
